41952a1bd45ab2a71ca1c2b8a5f126ab61f636f8
aghosh <aghosh@voysis.com>
2020-02-18 12:16:16 +0000

    recommendation added

--- src/main/java/com/mycompany/data/SpotifyManager.java
+++ src/main/java/com/mycompany/data/SpotifyManager.java
@@ -1,18 +1,46 @@
 package com.mycompany.data;
 
 import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
+import com.wrapper.spotify.model_objects.specification.AlbumSimplified;
+import com.wrapper.spotify.model_objects.specification.ArtistSimplified;
+import com.wrapper.spotify.model_objects.specification.Paging;
+import com.wrapper.spotify.model_objects.specification.Recommendations;
+import com.wrapper.spotify.model_objects.specification.TrackSimplified;
+import com.wrapper.spotify.requests.data.browse.GetRecommendationsRequest;
+import com.wrapper.spotify.requests.data.search.simplified.SearchAlbumsRequest;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SpotifyManager {
 
     private String client_id;
     private String client_secret;
-    private final SpotifyApi spotifyApi;
+    private SpotifyApi spotifyApi;
+    GetRecommendationsRequest.Builder gmb;
 
     public SpotifyManager(String client_id, String client_secret) {
         this.client_id = client_id;
         this.client_secret = client_secret;
-        this.spotifyApi = new SpotifyApi.Builder().setClientId(this.client_id).setClientSecret(this.client_secret).build();
+        this.authenticate();
+        this.gmb = spotifyApi.getRecommendations()
+                .limit(100)
+                .max_popularity(50)
+                .min_popularity(10)
+                .target_popularity(20);
+    }
 
+    private void authenticate() {
+        try {
+            this.spotifyApi = new SpotifyApi.Builder().setClientId(this.client_id).setClientSecret(this.client_secret).build();
+            ClientCredentials cc = this.spotifyApi.clientCredentials().build().execute();
+            this.spotifyApi.setAccessToken(cc.getAccessToken());
+
+        } catch (IOException | SpotifyWebApiException e) {
+            e.printStackTrace();
+        }
     }
 
     public SpotifyApi getSpotifyApi() {
@@ -33,5 +61,35 @@ public class SpotifyManager {
 
     public void setClient_secret(String client_secret) {
         this.client_secret = client_secret;
+    }
+
+    public void searchAlbum(String name) {
+        SearchAlbumsRequest album = this.spotifyApi.searchAlbums("The Joshua Tree").build();
+
+        try {
+            Paging<AlbumSimplified> albumSimplifiedPaging = album.execute();
+            for (AlbumSimplified i : albumSimplifiedPaging.getItems()) {
+                System.out.println("album::" + i.getName());
+            }
+
+        } catch (SpotifyWebApiException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getRecommendation(String genre) {
+        try {
+            this.gmb.seed_genres(genre);
+            Recommendations reco = this.gmb.build().execute();
+            for (TrackSimplified t : reco.getTracks()) {
+                for (ArtistSimplified a : t.getArtists()) {
+                    System.out.println(a.getName() + ":" + a.getId() + "\t" + t.getName() + ":" + t.getId() +"\t"+ t.getUri());
+                }
+            }
+
+        } catch (IOException | SpotifyWebApiException ex) {
+            Logger.getLogger(SpotifyManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 }
