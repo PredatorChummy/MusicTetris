a26e7029e2d5b26f363b15d12eafa2245726aba6
aghosh <aghosh@voysis.com>
2020-02-18 10:42:25 +0000

    update structure

--- src/main/java/com/mycompany/data/SpotifyManager.java
+++ src/main/java/com/mycompany/data/SpotifyManager.java
@@ -1,21 +1,37 @@
 package com.mycompany.data;
+
 import com.wrapper.spotify.SpotifyApi;
-import com.wrapper.spotify.SpotifyHttpManager;
-import com.wrapper.spotify.exceptions.SpotifyWebApiException;
-import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
-import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
-import com.wrapper.spotify.requests.data.albums.GetAlbumRequest;
 
 public class SpotifyManager {
 
-    public static void main(String[] args) {
-        SpotifyApi spotifyApi = new SpotifyApi.Builder().setClientId("").setClientSecret("").build();
-        GetAlbumRequest.Builder album = spotifyApi.getAlbum("The Joshua Tree");
-        GetAlbumRequest build = album.build();
-        System.out.println("album::"+album.build());
-//                .("2570579e804b4ae8ac410c01f3e592d8")
-//                .build();
-//        spotifyApi.getSomething("qKRpDADUKrFeKhFHDMdfcu").market(CountryCode.SE).build();
+    private String client_id;
+    private String client_secret;
+    private final SpotifyApi spotifyApi;
 
+    public SpotifyManager(String client_id, String client_secret) {
+        this.client_id = client_id;
+        this.client_secret = client_secret;
+        this.spotifyApi = new SpotifyApi.Builder().setClientId(this.client_id).setClientSecret(this.client_secret).build();
+
+    }
+
+    public SpotifyApi getSpotifyApi() {
+        return spotifyApi;
+    }
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getClient_secret() {
+        return client_secret;
+    }
+
+    public void setClient_secret(String client_secret) {
+        this.client_secret = client_secret;
     }
 }
